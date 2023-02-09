ad32e71615d76711872ec4db7db2fc1a4179bb60
Daniel.McIntyre <daniel-mcintyre@hotmail.com>
2023-02-09 11:06:37 -0500

    successful fetch of movies from movie db API

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,8 @@
 
-export default function Home() {
+export default async function Home() {
+  const data = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`)
+  const response = await data.json()
+  console.log(response)
   return (
     <main>
       <h1 className='text-lg'>Hello Next!</h1>
